46d27957978cc175130362a07664dbd88de43425
Rawan <51280491+RawanAlfarhan@users.noreply.github.com>
2020-08-03 21:53:22 +0300

    first commit

--- src/server/index.js
+++ src/server/index.js
@@ -36,19 +36,19 @@ app.use(express.static('dist'))
 
 console.log(__dirname)
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile('dist/index.html')
 })
 // designates what port the app will listen to for incoming requests
 app.listen(8080, function () {
     console.log('Example app listening on port 8080!')
 })
-app.post('/test', function (req, res) {
+app.post('/testing', function (req, res) {
     textapi.sentiment({
       'url': req.body.text
   }, function(error, response) {
+       if(!error){
        console.log(response);
-       res.send(response);
+       res.send(response);}
     
   });
 });
